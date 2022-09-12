5bdbc38b3056669ae00d93d12b0d2b802bec70e1
Ben Quinn <benjamesquinn@gmail.com>
2022-09-12 22:06:42 +0100

    Added TechStack component and styles

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../styles/body.css";
 import NavBar from "./NavBar";
 import AboutMe from "./AboutMe";
+import TechStack from "./TechStack";
 
 function Body() {
   return (
@@ -11,6 +12,7 @@ function Body() {
         <NavBar />
         <Routes>
           <Route exact path="/about-me" element={<AboutMe />} />
+          <Route exact path="/tech-stack" element={<TechStack />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/TechStack.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/tech-stack.css";
+
+function TechStack() {
+  return (
+    <div className="page">
+      <h3 className="page-title">TECH STACK</h3>
+      <ul className="tech-stack-list">
+        <li className="tech-stack-item">HTML5</li>
+        <li className="tech-stack-item">CSS</li>
+        <li className="tech-stack-item">JAVASCRIPT</li>
+        <li className="tech-stack-item">NODE.JS</li>
+        <li className="tech-stack-item">REACT</li>
+        <li className="tech-stack-item">DOCKER</li>
+        <li className="tech-stack-item">SQL</li>
+        <li className="tech-stack-item">MONGODB</li>
+        <li className="tech-stack-item">GIT</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TechStack;
